0163f3317705055e6c39cbbbdbc2178cfcb85f3f
aisyahdewih <aisyahdewih@gmail.com>
2020-03-25 18:22:32 +0700

    form absensi

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="container">
     <div>
-      <h1 class="title">badminton-apps</h1>
+      <logo />
+      <br />
+      <h4 class="title">
+        Aplikasi Perhitungan Score Badminton By Tetamba
+      </h4>
       <div>
-        <b-button v-b-modal.modal-prevent-closing pill variant="primary" size="lg">Guest</b-button>
-        <div class="mt-3">
-          <h4>Submitted Name :</h4>
-          <div v-for="guest in guests" v-bind:key="guest.id" class="collection-item">
-            <div variant="primary">{{guest.name}}</div>
-          </div>
-        </div>
+        <b-button variant="outline-primary" size="lg">Let's Count!</b-button>
+        <b-button
+          v-b-modal.modal-prevent-closing
+          variant="outline-success"
+          size="lg"
+          >Absensi</b-button
+        >
         <b-modal
           id="modal-prevent-closing"
           ref="modal"
-          title="Submit Your Name"  
+          title="Submit Your Name"
+          @show="resetModal"
+          @hidden="resetModal"
           @ok="handleOk"
         >
           <form ref="form" @submit.stop.prevent="handleSubmit">
@@ -23,50 +29,109 @@
               label-for="name-input"
               invalid-feedback="Name is required"
             >
-              <b-form-input id="name-input" v-model="name" :state="nameState" required></b-form-input>
+              <b-form-input
+                id="name-input"
+                v-model="name"
+                :state="nameState"
+                required
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group
+              :state="genderState"
+              label="gender"
+              label-for="gender-input"
+              invalid-feedback="Gender is required"
+            >
+              <b-form-select
+                id="gender-input"
+                v-model="gender"
+                :options="options"
+                class="mb-3"
+                value-field="value"
+                text-field="item"
+                disabled-field="notEnabled"
+                :state="genderState"
+                required
+              ></b-form-select>
+            </b-form-group>
+            <b-form-group
+              :state="dateState"
+              label="date"
+              label-for="datepicker-dateformat2"
+              invalid-feedback="Date is required"
+            >
+              <b-form-datepicker
+                id="datepicker-dateformat2"
+                v-model="date"
+                :date-format-options="{
+                  year: 'numeric',
+                  month: 'numeric',
+                  day: 'numeric'
+                }"
+                locale="en"
+                required
+              ></b-form-datepicker>
             </b-form-group>
           </form>
         </b-modal>
+        <b-button variant="outline-danger" size="lg">Help</b-button>
       </div>
     </div>
+
+    <h5 class="footer">
+      &copy;Tetamba Studio
+    </h5>
   </div>
 </template>
 
 <script>
 import db from "~/plugins/firebase";
+import Logo from "~/components/Logo.vue";
 
 export default {
-  components: {},
+  components: { Logo },
   data() {
     return {
       name: null,
+      gender: null,
+      date: null,
       nameState: null,
-      submittedNames: [],
-      guests: []
+      genderState: null,
+      dateState: null,
+
+      peoples: [],
+      options: [
+        { item: "Please select a gender", value: null },
+        { item: "Female", value: "F" },
+        { item: "Male", value: "M" }
+      ]
     };
   },
   mounted() {
-   this.getDataGuest()
+    this.getDataGuest();
   },
   methods: {
-    getDataGuest(){
-       db.collection("guests")
-      .orderBy("name")
-      .get()
-      .then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-          const data = {
-            id: doc.id,
-            guest_id: doc.data().guest_id,
-            name: doc.data().name
-          };
-          this.guests.push(data);
+    getDataGuest() {
+      db.collection("peoples")
+        .orderBy("name")
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            const data = {
+              id: doc.id,
+              name: doc.data().name,
+              gender: doc.data().gender,
+              date: doc.data().date
+            };
+            this.peoples.push(data);
+          });
         });
-      });
     },
     checkFormValidity() {
       const valid = this.$refs.form.checkValidity();
       this.nameState = valid;
+      this.genderState = valid;
+      this.dateState = valid;
       return valid;
     },
     handleOk(bvModalEvt) {
@@ -75,6 +140,14 @@ export default {
       // Trigger submit handler
       this.handleSubmit();
     },
+    resetModal() {
+      this.name = "";
+      this.nameState = null;
+      this.gender = "";
+      this.genderState = null;
+      this.date = "";
+      this.dateState = null;
+    },
     handleSubmit() {
       // Exit when the form isn't valid
       if (!this.checkFormValidity()) {
@@ -82,9 +155,11 @@ export default {
       }
       // Push the name to submitted names
       //this.submittedNames.push(this.name);
-      db.collection("guests")
+      db.collection("peoples")
         .add({
-          name: this.name
+          name: this.name,
+          gender: this.gender,
+          date: this.date
         })
         .then(docRef => this.$router.push("/"))
         .catch(error => console.log(err));
@@ -106,26 +181,24 @@ export default {
   align-items: center;
   text-align: center;
 }
-
 .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
     "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 30px;
   color: #35495e;
   letter-spacing: 1px;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  margin: 0 auto;
+  width: 100%;
+  text-align: center;
+  font-weight: 150;
+  font-size: 20px;
   color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,21 +1,22 @@
-import * as firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 // eslint-disable-next-line no-var
 var firebaseConfig = {
-    apiKey: 'AIzaSyDjXrhPT4OnndXi2gx8MPJteHhQF5NCPlQ',
-    authDomain: 'badminton-apps.firebaseapp.com',
-    databaseURL: 'https://badminton-apps.firebaseio.com',
-    projectId: 'badminton-apps',
-    storageBucket: 'badminton-apps.appspot.com',
-    messagingSenderId: '923029487950',
-    appId: '1:923029487950:web:938880332115f2f6941e1f',
-    measurementId: 'G-QS247VCNCH'
-}
+  apiKey: "AIzaSyBgF8AJ9SLu02a20s3kjduw6zUbVtzZ6l8",
+  authDomain: "badminton-tetamba-junior.firebaseapp.com",
+  databaseURL: "https://badminton-tetamba-junior.firebaseio.com",
+  projectId: "badminton-tetamba-junior",
+  storageBucket: "badminton-tetamba-junior.appspot.com",
+  messagingSenderId: "425610018769",
+  appId: "1:425610018769:web:ffe8f96168bb8fc7252af0",
+  measurementId: "G-SJJEJK5YSF"
+};
+
 // Initialize Firebase
 // eslint-disable-next-line no-unused-vars
-let app = null
+let app = null;
 if (!firebase.apps.length) {
-    app = firebase.initializeApp(firebaseConfig)
+  app = firebase.initializeApp(firebaseConfig);
 }
-export default firebase.firestore()
+export default firebase.firestore();
