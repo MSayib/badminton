b9adce8353a7eb7514d6a4ca41e04000064cf049
Muhammad Fikri <fikrimuhammad2016@gmail.com>
2020-03-25 22:23:16 +0700

    sedikit perubahan

---
+++ pages/start/index.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <b-container align="center" style="margin-top: 180px">
+      <Stopwatch />
+      <b-row>
+        <b-col>
+          <h4>Score Counter</h4>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>
+          <b-card border-variant="dark" header="Tim A" align="center">
+            <b-card-text class="scoreBoard">{{ teamA }}</b-card-text>
+          </b-card>
+        </b-col>
+        <b-col cols="4" style="margin-top: 40px">
+          <b-img :src="img" width="90%" fluid alt="Responsive image" />
+        </b-col>
+        <b-col>
+          <b-card border-variant="dark" header="Tim B" align="center">
+            <b-card-text class="scoreBoard">{{ teamB }}</b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>{{ status }} </b-col>
+      </b-row>
+
+      <b-row>
+        <!-- team A -->
+        <b-col>
+          <b-button @click="tambahTeamA" variant="primary">+</b-button>
+          <b-button @click="kurangTeamA" variant="danger">-</b-button>
+        </b-col>
+        <b-col> </b-col>
+        <!-- team B -->
+        <b-col>
+          <b-button @click="tambahTeamB" variant="primary">+</b-button>
+          <b-button @click="kurangTeamB" variant="danger">-</b-button>
+        </b-col>
+      </b-row>
+      <!-- reset button -->
+      <b-row style="padding: 0 10px;">
+        <b-col>
+          <b-button variant="primary" @click="resetScore" @resetTime="reset">Reset Score</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Stopwatch from "~/components/stopwatch";
+export default {
+  components: {
+    Stopwatch
+  },
+  data() {
+    return {
+      teamA: 0,
+      teamB: 0,
+      status: "",
+      img: "https://i.ya-webdesign.com/images/vs-png-5.png"
+    };
+  },
+  methods: {
+    tambahTeamA() {
+      this.teamA++;
+      if (this.teamA % 2 == 1) {
+        this.status = "Team A serve di kiri";
+      } else {
+        this.status = "Team A serve di kanan";
+      }
+    },
+    tambahTeamB() {
+      this.teamB++;
+      if (this.teamB % 2 == 1) {
+        this.status = "Team B serve di kiri";
+      } else {
+        this.status = "Team B serve di kanan";
+      }
+    },
+    kurangTeamA() {
+      this.teamA -= 1;
+    },
+    kurangTeamB() {
+      this.teamB -= 1;
+    },
+    resetScore() {
+      (this.teamA = 0), (this.teamB = 0);
+    },
+    resetData() {
+      (this.teamA = 0), (this.teamB = 0), this.$router.push("/start");
+    },
+  }
+};
+</script>
+
+<style>
+.scoreBoard {
+  font-size: 60px;
+}
+.score {
+  border: 2px solid;
+}
+</style>
