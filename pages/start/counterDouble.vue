c6060cb7dabb971b929d958f9a1b4a983b47407b
Muhammad Fikri <fikrimuhammad2016@gmail.com>
2020-03-27 13:18:22 +0700

    Fix logic Match Point dan Deuce by fikri

--- pages/start/counterDouble.vue
+++ pages/start/counterDouble.vue
@@ -15,7 +15,11 @@
         </b-row>
         <b-row>
           <b-col>
-            <b-card border-variant="dark" header="Player A1 / Player A2" align="center">
+            <b-card
+              border-variant="dark"
+              header="Player A1 / Player A2"
+              align="center"
+            >
               <b-card-text class="scoreBoard">{{ playerA }}</b-card-text>
             </b-card>
           </b-col>
@@ -23,15 +27,15 @@
             <b-img :src="img" width="90%" fluid alt="Responsive image" />
           </b-col>
           <b-col>
-            <b-card border-variant="dark" header="Player B1 / Player B2" align="center">
+            <b-card
+              border-variant="dark"
+              header="Player B1 / Player B2"
+              align="center"
+            >
               <b-card-text class="scoreBoard">{{ playerB }}</b-card-text>
             </b-card>
           </b-col>
         </b-row>
-
-        <!-- <b-row>
-          <b-col>{{ isDeuce ? "Deuce" : "Bukan" }}</b-col>
-        </b-row>-->
         <b-row>
           <b-col>isKananA {{ isKananA }} || isKananB {{ isKananB }}</b-col>
           <b-col>{{ status }}</b-col>
@@ -53,7 +57,9 @@
         <!-- reset button -->
         <b-row style="padding: 0 10px;">
           <b-col>
-            <b-button variant="primary" @click="resetScore">Reset Score</b-button>
+            <b-button variant="primary" @click="resetScore"
+              >Reset Score</b-button
+            >
           </b-col>
         </b-row>
       </b-container>
@@ -70,8 +76,8 @@ export default {
   },
   data() {
     return {
-      playerA: 0,
-      playerB: 0,
+      playerA: 29,
+      playerB: 29,
       img: "https://i.ya-webdesign.com/images/vs-png-5.png",
       isServeA: 0,
       isServeB: 0
@@ -84,6 +90,53 @@ export default {
     },
     isKananB() {
       return this.isServeB % 2 === 1;
+    },
+    isMatchPoint() {
+      if (this.playerA <= 21 && this.playerB <= 21) {
+        if (
+          (this.playerA === 20 && this.playerB !== 20) ||
+          (this.playerA !== 20 && this.playerB == 20)
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      } else if (this.playerA === 29 && this.playerB === 29) {
+        return true;
+      } else {
+        if (Math.abs(this.playerA - this.playerB) === 1) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    isDeuce() {
+      if (this.playerA >= 20 && this.playerB >= 20) {
+        if (this.playerA === this.playerB) {
+          return true;
+        }
+      }
+
+      return false;
+    },
+    isMenangA(){
+      if (this.playerA >= 21){
+        if (this.playerA >= this.playerB + 2){
+          return true
+        }else if(this.playerA === 30){
+          return true
+        }     
+      }
+    },
+    isMenangB(){
+      if (this.playerB >= 21){
+        if(this.playerB >= this.playerA +2){
+          return true
+        }else if(this.playerB === 30){
+          return true
+        }
+      }
     },
     status() {
       const teamAServe = this.isServeA % 2 === 0 ? "A1" : "A2";
@@ -99,6 +152,26 @@ export default {
   },
 
   watch: {
+    isMatchPoint(val) {
+      if (val) {
+        alert("Match Point!");
+      }
+    },
+    isDeuce(val) {
+      if (val) {
+        alert("Deuce!");
+      }
+    },
+    isMenangA(val){
+      if (val){
+        alert("Tim A Menang!")
+      }
+    },
+    isMenangB(val){
+      if (val){
+        alert("Tim B Menang")
+      }
+    },
     playerA() {
       if (this.isKananA && !this.isKananB) {
         return;
