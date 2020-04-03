0335aa86a5bb2334c0ad7fc09fd72cee6c5dc17f
aisyahdewih <aisyahdewih@gmail.com>
2020-04-03 19:11:31 +0700

    tambah history/documentation image

--- pages/history/index.vue
+++ pages/history/index.vue
@@ -2,6 +2,16 @@
   <div>
     <Navbar />
     <h3>history</h3>
+    <b-row class="p-5"> 
+      <b-col class="p-3" cols="6" md="4" v-for="item in items" :key="item.id">
+          <b-card :title="item.partai" :img-src="item.images" img-alt="Image" img-top width="80px">
+            <b-card-text>this</b-card-text>
+            <template v-slot:footer>
+              <small class="text-muted">{{item.created_at}}</small>
+            </template>
+          </b-card> 
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -16,8 +26,7 @@ export default {
   components: { Navbar },
   data() {
     return {
-    
-     
+      items: []
     };
   },
 
@@ -36,9 +45,26 @@ export default {
     }
   },
   methods: {
-    
+    readData() {
+      db.collection("pertandingan")
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            // doc.data() is never undefined for query doc snapshots
+            this.items.push({
+              id: doc.id,
+              partai: doc.data().partai,
+              images: doc.data().images,
+              created_at: moment(doc.data().created_at).format("LLLL")
+            });
+            console.log(doc.id, " => ", doc.data());
+          });
+        });
+    }
   },
- 
+  created() {
+    this.readData();
+  }
 };
 </script>
 
